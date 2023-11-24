857d0653c805f575ff04dc89072aaf7e3f07a21e
baiwusanyu-c <740132583@qq.com>
2023-11-24 16:39:56 +0800

    test: updated unit test

---
+++ packages/compiler-vapor/__tests__/fixtures/singleRoot.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+const handleClick = () => count.value++
+</script>
+
+<template>
+  <button @click="handleClick" :id="count">
+    {{count}}
+  </button>
+</template>
