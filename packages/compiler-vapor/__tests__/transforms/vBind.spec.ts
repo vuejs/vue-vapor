936c5ec7ac9456333ff9d28d7f4178bffddce879
Rizumu Ayaka <rizumu@ayaka.moe>
2023-12-09 20:43:43 +0800

    test: combine with transform and codegen tests

--- packages/compiler-vapor/__tests__/transforms/vBind.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/vBind.spec.ts
@@ -1,9 +1,4 @@
-import {
-  type RootNode,
-  ErrorCodes,
-  NodeTypes,
-  BindingTypes,
-} from '@vue/compiler-dom'
+import { ErrorCodes, NodeTypes, BindingTypes } from '@vue/compiler-dom'
 import {
   type RootIRNode,
   type CompilerOptions,
@@ -13,35 +8,35 @@ import {
   transformElement,
   IRNodeTypes,
   compile as _compile,
+  generate,
 } from '../../src'
 
-function parseWithVBind(
+function compileWithVBind(
   template: string,
   options: CompilerOptions = {},
-): RootIRNode {
-  const ast = parse(template)
+): {
+  ir: RootIRNode
+  code: string
+} {
+  const ast = parse(template, { prefixIdentifiers: true, ...options })
+
   const ir = transform(ast, {
     nodeTransforms: [transformElement],
     directiveTransforms: {
       bind: transformVBind,
     },
-    ...options,
-  })
-  return ir
-}
-
-function compile(template: string | RootNode, options: CompilerOptions = {}) {
-  let { code } = _compile(template, {
-    ...options,
-    mode: 'module',
     prefixIdentifiers: true,
+    ...options,
   })
-  return code
+
+  const { code } = generate(ir, { prefixIdentifiers: true, ...options })
+
+  return { ir, code }
 }
 
 describe('compiler: transform v-bind', () => {
   test('basic', () => {
-    const node = parseWithVBind(`<div v-bind:id="id"/>`)
+    const { ir: node, code } = compileWithVBind(`<div v-bind:id="id"/>`)
 
     expect(node.dynamic.children[0]).toMatchObject({
       id: 1,
@@ -89,10 +84,12 @@ describe('compiler: transform v-bind', () => {
         },
       ],
     })
+
+    expect(code).matchSnapshot()
   })
 
   test('no expression', () => {
-    const node = parseWithVBind(`<div v-bind:id />`)
+    const { ir: node, code } = compileWithVBind(`<div v-bind:id />`)
 
     expect(node.effect[0].operations[0]).toMatchObject({
       type: IRNodeTypes.SET_PROP,
@@ -113,26 +110,34 @@ describe('compiler: transform v-bind', () => {
         },
       },
     })
+
+    expect(code).matchSnapshot()
+    expect(code).contains('_setAttr(n1, "id", undefined, _ctx.id)')
   })
 
   test('no expression (shorthand)', () => {
-    const node = parseWithVBind(`<div :id />`)
+    const { ir: node, code } = compileWithVBind(`<div :camel-case />`)
 
     expect(node.effect[0].operations[0]).toMatchObject({
       type: IRNodeTypes.SET_PROP,
       key: {
-        content: `id`,
+        content: `camel-case`,
         isStatic: true,
       },
       value: {
-        content: `id`,
+        content: `camelCase`,
         isStatic: false,
       },
     })
+
+    expect(code).matchSnapshot()
+    expect(code).contains(
+      '_setAttr(n1, "camel-case", undefined, _ctx.camelCase)',
+    )
   })
 
   test('dynamic arg', () => {
-    const node = parseWithVBind(`<div v-bind:[id]="id"/>`)
+    const { ir: node, code } = compileWithVBind(`<div v-bind:[id]="id"/>`)
     expect(node.effect[0].operations[0]).toMatchObject({
       type: IRNodeTypes.SET_PROP,
       element: 1,
@@ -147,11 +152,17 @@ describe('compiler: transform v-bind', () => {
         isStatic: false,
       },
     })
+
+    expect(code).matchSnapshot()
+    expect(code).contains('_setAttr(n1, _ctx.id, undefined, _ctx.id)')
   })
 
   test('should error if empty expression', () => {
     const onError = vi.fn()
-    const node = parseWithVBind(`<div v-bind:arg="" />`, { onError })
+    const { ir: node, code } = compileWithVBind(`<div v-bind:arg="" />`, {
+      onError,
+    })
+
     expect(onError.mock.calls[0][0]).toMatchObject({
       code: ErrorCodes.X_V_BIND_NO_EXPRESSION,
       loc: {
@@ -163,10 +174,16 @@ describe('compiler: transform v-bind', () => {
       type: IRNodeTypes.TEMPLATE_FACTORY,
       template: '<div arg=""></div>',
     })
+
+    expect(code).matchSnapshot()
+    expect(code).contains(JSON.stringify('<div arg=""></div>'))
   })
 
   test('.camel modifier', () => {
-    const node = parseWithVBind(`<div v-bind:foo-bar.camel="id"/>`)
+    const { ir: node, code } = compileWithVBind(
+      `<div v-bind:foo-bar.camel="id"/>`,
+    )
+
     expect(node.effect[0].operations[0]).toMatchObject({
       key: {
         content: `fooBar`,
@@ -177,10 +194,14 @@ describe('compiler: transform v-bind', () => {
         isStatic: false,
       },
     })
+
+    expect(code).matchSnapshot()
+    expect(code).contains('_setAttr(n1, "fooBar", undefined, _ctx.id)')
   })
 
   test('.camel modifier w/ no expression', () => {
-    const node = parseWithVBind(`<div v-bind:foo-bar.camel />`)
+    const { ir: node, code } = compileWithVBind(`<div v-bind:foo-bar.camel />`)
+
     expect(node.effect[0].operations[0]).toMatchObject({
       key: {
         content: `fooBar`,
@@ -191,10 +212,17 @@ describe('compiler: transform v-bind', () => {
         isStatic: false,
       },
     })
+
+    expect(code).matchSnapshot()
+    expect(code).contains('effect')
+    expect(code).contains('_setAttr(n1, "fooBar", undefined, _ctx.fooBar)')
   })
 
   test('.camel modifier w/ dynamic arg', () => {
-    const node = parseWithVBind(`<div v-bind:[foo].camel="id"/>`)
+    const { ir: node, code } = compileWithVBind(
+      `<div v-bind:[foo].camel="id"/>`,
+    )
+
     expect(node.effect[0].operations[0]).toMatchObject({
       runtimeCamelize: true,
       key: {
@@ -206,6 +234,12 @@ describe('compiler: transform v-bind', () => {
         isStatic: false,
       },
     })
+
+    expect(code).matchSnapshot()
+    expect(code).contains('effect')
+    expect(code).contains(
+      `_setAttr(n1, _camelize(_ctx.foo), undefined, _ctx.id)`,
+    )
   })
 
   test.todo('.camel modifier w/ dynamic arg + prefixIdentifiers')
@@ -218,81 +252,4 @@ describe('compiler: transform v-bind', () => {
   test.todo('.prop modifier (shortband) w/ no expression')
   test.todo('.attr modifier')
   test.todo('.attr modifier w/ no expression')
-})
-
-// TODO: combine with above
-describe('compiler: codegen v-bind', () => {
-  test('simple expression', () => {
-    const code = compile(`<div :id="id"></div>`, {
-      bindingMetadata: {
-        id: BindingTypes.SETUP_REF,
-      },
-    })
-    expect(code).matchSnapshot()
-  })
-
-  test('should error if no expression', () => {
-    const onError = vi.fn()
-    const code = compile(`<div v-bind:arg="" />`, { onError })
-
-    expect(onError.mock.calls[0][0]).toMatchObject({
-      code: ErrorCodes.X_V_BIND_NO_EXPRESSION,
-      loc: {
-        start: {
-          line: 1,
-          column: 6,
-        },
-        end: {
-          line: 1,
-          column: 19,
-        },
-      },
-    })
-
-    expect(code).matchSnapshot()
-    // the arg is static
-    expect(code).contains(JSON.stringify('<div arg=""></div>'))
-  })
-
-  test('no expression', () => {
-    const code = compile('<div v-bind:id />', {
-      bindingMetadata: {
-        id: BindingTypes.SETUP_REF,
-      },
-    })
-
-    expect(code).matchSnapshot()
-    expect(code).contains('_setAttr(n1, "id", undefined, _ctx.id)')
-  })
-
-  test('no expression (shorthand)', () => {
-    const code = compile('<div :camel-case />', {
-      bindingMetadata: {
-        camelCase: BindingTypes.SETUP_REF,
-      },
-    })
-
-    expect(code).matchSnapshot()
-    expect(code).contains(
-      '_setAttr(n1, "camel-case", undefined, _ctx.camelCase)',
-    )
-  })
-
-  test('dynamic arg', () => {
-    const code = compile('<div v-bind:[id]="id"/>', {
-      bindingMetadata: {
-        id: BindingTypes.SETUP_REF,
-      },
-    })
-
-    expect(code).matchSnapshot()
-    expect(code).contains('_setAttr(n1, _ctx.id, undefined, _ctx.id)')
-  })
-
-  test('.camel modifier', () => {
-    const code = compile(`<div v-bind:foo-bar.camel="id"/>`)
-
-    expect(code).matchSnapshot()
-    expect(code).contains('fooBar')
-  })
 })
