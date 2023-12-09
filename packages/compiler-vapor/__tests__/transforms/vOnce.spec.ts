a915195c2d91f791a3ef89521b38460c96930c70
Rizumu Ayaka <rizumu@ayaka.moe>
2023-12-09 16:34:25 +0800

    test: refactor v-once test to ir

--- packages/compiler-vapor/__tests__/transforms/vOnce.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/vOnce.spec.ts
@@ -1,18 +1,32 @@
-import { type RootNode, BindingTypes } from '@vue/compiler-dom'
-import { type CompilerOptions, compile as _compile } from '../../src'
+import { BindingTypes, parse } from '@vue/compiler-dom'
+import {
+  type CompilerOptions,
+  compile as _compile,
+  transform,
+  generate as generate,
+  IRNodeTypes,
+} from '../../src'
+import { getBaseTransformPreset } from '../../src/compile'
 
-function compile(template: string | RootNode, options: CompilerOptions = {}) {
-  let { code } = _compile(template, {
-    ...options,
-    mode: 'module',
+function transformWithOnce(template: string, options: CompilerOptions = {}) {
+  const ast = parse(template, {
     prefixIdentifiers: true,
+    ...options,
   })
-  return code
+  const [nodeTransforms, directiveTransforms] = getBaseTransformPreset(true)
+
+  const ir = transform(ast, {
+    nodeTransforms,
+    directiveTransforms,
+    prefixIdentifiers: true,
+    ...options,
+  })
+  return ir
 }
 
-describe('v-once', () => {
+describe('compiler: v-once transform', () => {
   test('basic', () => {
-    const code = compile(
+    const root = transformWithOnce(
       `<div v-once>
         {{ msg }}
         <span :class="clz" />
@@ -24,12 +38,35 @@ describe('v-once', () => {
         },
       },
     )
-    expect(code).matchSnapshot()
+
+    const { code } = generate(root, { prefixIdentifiers: true })
+    expect(code).toMatchSnapshot()
   })
 
   test('as root node', () => {
-    const code = compile(`<div :id="foo" v-once />`)
+    const root = transformWithOnce(`<div :id="foo" v-once />`)
+
+    expect(root.helpers.size).toBe(0)
+    expect(root.vaporHelpers.size).toBe(0)
+    expect(root.effect).toMatchObject([])
+    expect(root.operation.length).toBe(1)
+
+    expect(root.operation[0]).toMatchObject({
+      type: IRNodeTypes.SET_PROP,
+      element: 1,
+      name: {
+        type: IRNodeTypes.SET_TEXT,
+        content: 'id',
+        isStatic: true,
+      },
+      value: {
+        type: IRNodeTypes.SET_TEXT,
+        content: 'foo',
+        isStatic: false,
+      },
+    })
+
+    const { code } = generate(root, { prefixIdentifiers: true })
     expect(code).toMatchSnapshot()
-    expect(code).not.contains('effect')
   })
 })
