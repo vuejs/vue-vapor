52b00fdc738c09831cbc395d6e848922ae8e8e9f
Ubugeeei <ubuge1122@gmail.com>
2024-01-03 21:43:07 +0900

    test(runtime-vapor): should render props value (object spec)

--- packages/runtime-vapor/__tests__/componentProps.spec.ts
+++ packages/runtime-vapor/__tests__/componentProps.spec.ts
@@ -15,10 +15,64 @@ beforeEach(() => initHost())
 afterEach(() => host.remove())
 
 describe('runtime: compoentn props', () => {
-  // pending: https://github.com/vuejs/core-vapor/issues/84
+  // TODO: pending: https://github.com/vuejs/core-vapor/issues/84
   // test('should render props value (string array spec)', () => {
   //   const ChildComp = defineComponent({
   //     props: ['foo'],
+  //     setup() {
+  //       const __returned__ = {}
+  //       Object.defineProperty(__returned__, '__isScriptSetup', {
+  //         enumerable: false,
+  //         value: true,
+  //       })
+  //       return __returned__
+  //     },
+  //     render(_ctx: any) {
+  //       const t0 = template('<div></div>')
+  //       const n0 = t0()
+  //       const {
+  //         0: [n1],
+  //       } = children(n0)
+  //       watchEffect(() => {
+  //         setText(n1, void 0, _ctx.foo)
+  //       })
+  //       return n0
+  //     },
+  //   })
+  //   const Comp = defineComponent({
+  //     setup() {
+  //       const __returned__ = {}
+  //       Object.defineProperty(__returned__, '__isScriptSetup', {
+  //         enumerable: false,
+  //         value: true,
+  //       })
+  //       return __returned__
+  //     },
+  //     render() {
+  //       const t0 = template('<div></div>')
+  //       const n0 = t0()
+  //       const {
+  //         0: [n1],
+  //       } = children(n0)
+  //       renderComponent(
+  //         ChildComp as any,
+  //         {
+  //           get foo() {
+  //             return 'foo'
+  //           },
+  //         },
+  //         n1 as any,
+  //       )
+  //       return n0
+  //     },
+  //   })
+  //   renderComponent(Comp as any, {}, '#host')
+  //   expect(host.innerHTML).toBe('<div><div>foo</div></div>')
+  // })
+  // TODO: pending: https://github.com/vuejs/core-vapor/issues/84
+  // test('should render props value (object spec)', () => {
+  //   const ChildComp = defineComponent({
+  //     props: { foo: {} },
   //     setup() {
   //       const __returned__ = {}
   //       Object.defineProperty(__returned__, '__isScriptSetup', {
