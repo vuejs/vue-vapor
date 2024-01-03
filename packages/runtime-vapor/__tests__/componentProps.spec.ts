dd5d75b98d34b3d5e9ce9396f99fce4cb56ee585
Ubugeeei <ubuge1122@gmail.com>
2024-01-03 21:53:27 +0900

    test(runtime-vapor): should render computed props

--- packages/runtime-vapor/__tests__/componentProps.spec.ts
+++ packages/runtime-vapor/__tests__/componentProps.spec.ts
@@ -1,8 +1,9 @@
-import { defineComponent, ref, watchEffect } from 'vue'
+import { computed, defineComponent, ref, watchEffect } from 'vue'
 
 import { template } from '../src/template'
 import { children, setText } from '../src/dom'
 import { render as renderComponent } from '../src/render'
+import { nextTick } from '../src/scheduler'
 
 let host: HTMLElement
 
@@ -235,4 +236,66 @@ describe('runtime: compoentn props', () => {
   //   increment() // update state
   //   expect(host.innerHTML).toBe('<div><div>1</div></div>')
   // })
+
+  test('should render computed props', async () => {
+    const ChildComp = defineComponent({
+      props: { count: { type: Number, required: true } },
+      setup(props) {
+        const double = computed(() => props.count * 2)
+        const __returned__ = { double }
+        Object.defineProperty(__returned__, '__isScriptSetup', {
+          enumerable: false,
+          value: true,
+        })
+        return __returned__
+      },
+      render(_ctx: any) {
+        const t0 = template('<div></div>')
+        const n0 = t0()
+        const {
+          0: [n1],
+        } = children(n0)
+        watchEffect(() => {
+          setText(n1, void 0, _ctx.double)
+        })
+        return n0
+      },
+    })
+
+    const count = ref(0) // state
+    const increment = () => count.value++
+
+    const Comp = defineComponent({
+      setup() {
+        const __returned__ = { count }
+        Object.defineProperty(__returned__, '__isScriptSetup', {
+          enumerable: false,
+          value: true,
+        })
+        return __returned__
+      },
+      render(_ctx: any) {
+        const t0 = template('<div></div>')
+        const n0 = t0()
+        const {
+          0: [n1],
+        } = children(n0)
+        renderComponent(
+          ChildComp as any,
+          {
+            get count() {
+              return _ctx.count
+            },
+          },
+          n1 as any,
+        )
+        return n0
+      },
+    })
+    renderComponent(Comp as any, {}, '#host')
+    expect(host.innerHTML).toBe('<div><div>0</div></div>')
+    increment() // update state
+    await nextTick()
+    expect(host.innerHTML).toBe('<div><div>2</div></div>')
+  })
 })
