8e03c05909c7a68ab74bdba4a85bd94c3cdf6698
Ubugeeei <ubuge1122@gmail.com>
2024-01-03 21:44:57 +0900

    test(runtime-vapor): should render props default value

--- packages/runtime-vapor/__tests__/componentProps.spec.ts
+++ packages/runtime-vapor/__tests__/componentProps.spec.ts
@@ -123,4 +123,58 @@ describe('runtime: compoentn props', () => {
   //   renderComponent(Comp as any, {}, '#host')
   //   expect(host.innerHTML).toBe('<div><div>foo</div></div>')
   // })
+  // pending: https://github.com/vuejs/core-vapor/issues/84
+  // test('should render props default value', () => {
+  //   const ChildComp = defineComponent({
+  //     props: {
+  //       foo: { default: 'foo' }, // default value
+  //     },
+  //     setup() {
+  //       const __returned__ = {}
+  //       Object.defineProperty(__returned__, '__isScriptSetup', {
+  //         enumerable: false,
+  //         value: true,
+  //       })
+  //       return __returned__
+  //     },
+  //     render(_ctx: any) {
+  //       const t0 = template('<div></div>')
+  //       const n0 = t0()
+  //       const {
+  //         0: [n1],
+  //       } = children(n0)
+  //       watchEffect(() => {
+  //         setText(n1, void 0, _ctx.foo)
+  //       })
+  //       return n0
+  //     },
+  //   })
+  //   const Comp = defineComponent({
+  //     setup() {
+  //       const __returned__ = {}
+  //       Object.defineProperty(__returned__, '__isScriptSetup', {
+  //         enumerable: false,
+  //         value: true,
+  //       })
+  //       return __returned__
+  //     },
+  //     render() {
+  //       const t0 = template('<div></div>')
+  //       const n0 = t0()
+  //       const {
+  //         0: [n1],
+  //       } = children(n0)
+  //       renderComponent(
+  //         ChildComp as any,
+  //         {
+  //           /** no props are provided */
+  //         },
+  //         n1 as any,
+  //       )
+  //       return n0
+  //     },
+  //   })
+  //   renderComponent(Comp as any, {}, '#host')
+  //   expect(host.innerHTML).toBe('<div><div>foo</div></div>')
+  // })
 })
