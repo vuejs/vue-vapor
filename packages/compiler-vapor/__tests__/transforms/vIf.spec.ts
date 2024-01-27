60196cb3c852bdbfbaf8053850fabc0f528cdd56
Rizumu Ayaka <rizumu@ayaka.moe>
2024-01-28 00:31:54 +0800

    test: add some unit test

--- packages/compiler-vapor/__tests__/transforms/vIf.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/vIf.spec.ts
@@ -1,13 +1,45 @@
 import { makeCompile } from './_utils'
-import { transformElement, transformVIf } from '../../src'
+import {
+  transformElement,
+  transformInterpolation,
+  transformOnce,
+  transformVIf,
+  transformVText,
+} from '../../src'
 
 const compileWithVIf = makeCompile({
-  nodeTransforms: [transformElement, transformVIf],
+  nodeTransforms: [
+    transformOnce,
+    transformInterpolation,
+    transformVIf,
+    transformElement,
+  ],
+  directiveTransforms: {
+    text: transformVText,
+  },
 })
 
 describe('compiler: v-if', () => {
   test('basic v-if', () => {
-    const { code } = compileWithVIf(`<div v-if="ok"/>`)
+    const { code } = compileWithVIf(`<div v-if="ok">{{msg}}</div>`)
     expect(code).matchSnapshot()
   })
+
+  test('template v-if', () => {
+    const { code } = compileWithVIf(
+      `<template v-if="ok"><div/>hello<p v-text="msg"/></template>`,
+    )
+
+    expect(code).matchSnapshot()
+  })
+
+  test.todo('v-if with v-once')
+  test.todo('component v-if')
+  test.todo('v-if + v-else')
+  test.todo('v-if + v-else-if')
+  test.todo('v-if + v-else-if + v-else')
+  test.todo('comment between branches')
+  describe.todo('errors')
+  describe.todo('codegen')
+  test.todo('v-on with v-if')
 })
