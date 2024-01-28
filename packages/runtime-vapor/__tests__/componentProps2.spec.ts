22c7964bdedaddd0dc3c9d10297e986a4c1606e6
Ubugeeei <ubuge1122@gmail.com>
2024-01-28 23:37:13 +0900

    test(runtime-vapor): component props (functional)

--- packages/runtime-vapor/__tests__/componentProps2.spec.ts
+++ packages/runtime-vapor/__tests__/componentProps2.spec.ts
@@ -1,4 +1,6 @@
 import { defineComponent } from 'vue'
+
+import type { FunctionalComponent } from '../src/component'
 import { getCurrentInstance } from '../src/component'
 import { render } from '../src/render'
 
@@ -63,17 +65,86 @@ describe('component props (vapor)', () => {
     expect(props.fooBar).toEqual(3)
     expect(props.barBaz).toEqual(5)
 
+    render(Comp as any, {}, host)
+    expect(props.fooBar).toBeUndefined()
+    expect(props.barBaz).toBeUndefined()
+    // expect(props.qux).toEqual(5) // TODO: attrs
+  })
+
+  test('stateful with setup', () => {
+    // FIXME: is it necessary?
+  })
+
+  test('functional with declaration', () => {
+    let props: any
+    // TODO: attrs
+
+    const Comp: FunctionalComponent = (_props) => {
+      const instance = getCurrentInstance()!
+      props = instance.props
+      return {}
+    }
+    Comp.props = ['foo']
+    Comp.render = (() => {}) as any
+
     render(
       Comp as any,
       {
-        get qux() {
-          return 5
+        get foo() {
+          return 1
         },
       },
       host,
     )
-    expect(props.fooBar).toBeUndefined()
-    expect(props.barBaz).toBeUndefined()
-    // expect(props.qux).toEqual(5) // TODO: attrs
+    expect(props.foo).toEqual(1)
+
+    render(
+      Comp as any,
+      {
+        get foo() {
+          return 2
+        },
+      },
+      host,
+    )
+    expect(props.foo).toEqual(2)
+
+    render(Comp as any, {}, host)
+    expect(props.foo).toBeUndefined()
+  })
+
+  test('functional without declaration', () => {
+    let props: any
+    // TODO: attrs
+
+    const Comp: FunctionalComponent = (_props) => {
+      const instance = getCurrentInstance()!
+      props = instance.props
+      return {}
+    }
+    Comp.props = undefined as any
+    Comp.render = (() => {}) as any
+
+    render(
+      Comp as any,
+      {
+        get foo() {
+          return 1
+        },
+      },
+      host,
+    )
+    expect(props.foo).toBeUndefined()
+
+    render(
+      Comp as any,
+      {
+        get foo() {
+          return 2
+        },
+      },
+      host,
+    )
+    expect(props.foo).toBeUndefined()
   })
 })
