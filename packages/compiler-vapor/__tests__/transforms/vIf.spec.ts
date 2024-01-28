af5483419a4928b507d27c0041d749e378ed90f2
Rizumu Ayaka <rizumu@ayaka.moe>
2024-01-28 14:55:43 +0800

    test: add some test to v-if

--- packages/compiler-vapor/__tests__/transforms/vIf.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/vIf.spec.ts
@@ -1,11 +1,14 @@
 import { makeCompile } from './_utils'
 import {
+  IRNodeTypes,
+  type IfIRNode,
   transformElement,
   transformInterpolation,
   transformOnce,
   transformVIf,
   transformVText,
 } from '../../src'
+import { NodeTypes } from '@vue/compiler-core'
 
 const compileWithVIf = makeCompile({
   nodeTransforms: [
@@ -21,15 +24,87 @@ const compileWithVIf = makeCompile({
 
 describe('compiler: v-if', () => {
   test('basic v-if', () => {
-    const { code } = compileWithVIf(`<div v-if="ok">{{msg}}</div>`)
+    const { code, vaporHelpers, ir, helpers } = compileWithVIf(
+      `<div v-if="ok">{{msg}}</div>`,
+    )
+
+    expect(vaporHelpers).contains('createIf')
+    expect(helpers.size).toBe(0)
+
+    expect(ir.template).lengthOf(2)
+    expect(ir.template).toMatchObject([
+      {
+        template: '<div></div>',
+        type: IRNodeTypes.TEMPLATE_FACTORY,
+      },
+      {
+        type: IRNodeTypes.FRAGMENT_FACTORY,
+      },
+    ])
+    expect(ir.operation).toMatchObject([
+      {
+        type: IRNodeTypes.IF,
+        id: 1,
+        condition: {
+          type: NodeTypes.SIMPLE_EXPRESSION,
+          content: 'ok',
+          isStatic: false,
+        },
+        positive: {
+          type: IRNodeTypes.BLOCK_FUNCTION,
+          templateIndex: 0,
+        },
+      },
+      {
+        type: IRNodeTypes.APPEND_NODE,
+        elements: [1],
+        parent: 0,
+      },
+    ])
+
+    expect(ir.dynamic).toMatchObject({
+      id: 0,
+      children: { 0: { id: 1 } },
+    })
+
+    expect(ir.effect).toEqual([])
+    expect((ir.operation[0] as IfIRNode).positive.effect).lengthOf(1)
+
     expect(code).matchSnapshot()
   })
 
   test('template v-if', () => {
-    const { code } = compileWithVIf(
+    const { code, ir } = compileWithVIf(
       `<template v-if="ok"><div/>hello<p v-text="msg"/></template>`,
     )
     expect(code).matchSnapshot()
+
+    expect(ir.template).lengthOf(2)
+    expect(ir.template[0]).toMatchObject({
+      template: '<div></div>hello<p></p>',
+      type: IRNodeTypes.TEMPLATE_FACTORY,
+    })
+
+    expect(ir.effect).toEqual([])
+    expect((ir.operation[0] as IfIRNode).positive.effect).toMatchObject([
+      {
+        operations: [
+          {
+            type: IRNodeTypes.SET_TEXT,
+            element: 3,
+            value: {
+              content: 'msg',
+              type: NodeTypes.SIMPLE_EXPRESSION,
+              isStatic: false,
+            },
+          },
+        ],
+      },
+    ])
+    expect((ir.operation[0] as IfIRNode).positive.dynamic).toMatchObject({
+      id: 2,
+      children: { 2: { id: 3 } },
+    })
   })
 
   test('dedupe same template', () => {
@@ -42,7 +117,48 @@ describe('compiler: v-if', () => {
 
   test.todo('v-if with v-once')
   test.todo('component v-if')
-  test.todo('v-if + v-else')
+
+  test.fails('v-if + v-else', () => {
+    const { code, ir, vaporHelpers, helpers } = compileWithVIf(
+      `<div v-if="ok"/><p v-else/>`,
+    )
+    expect(code).matchSnapshot()
+    expect(ir.template).lengthOf(3)
+    expect(ir.template).toMatchObject([
+      {
+        template: '<div></div>',
+        type: IRNodeTypes.TEMPLATE_FACTORY,
+      },
+      {
+        template: '<p></p>',
+        type: IRNodeTypes.TEMPLATE_FACTORY,
+      },
+      {
+        type: IRNodeTypes.FRAGMENT_FACTORY,
+      },
+    ])
+
+    expect(ir.effect).toEqual([])
+
+    expect(vaporHelpers).contains('createIf')
+    expect(helpers.size).toBe(0)
+
+    expect(ir.operation).toMatchObject([
+      {
+        type: IRNodeTypes.IF,
+        id: 1,
+        positive: {
+          type: IRNodeTypes.BLOCK_FUNCTION,
+          templateIndex: 0,
+        },
+        negative: {
+          type: IRNodeTypes.BLOCK_FUNCTION,
+          templateIndex: 1,
+        },
+      },
+    ])
+  })
+
   test.todo('v-if + v-else-if')
   test.todo('v-if + v-else-if + v-else')
   test.todo('comment between branches')
