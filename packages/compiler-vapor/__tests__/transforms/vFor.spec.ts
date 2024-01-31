a7dcf9694f118511a798a9a2e7b9f3c0d354fc45
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-01-31 16:43:27 +0800

    test: update

--- packages/compiler-vapor/__tests__/transforms/vFor.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/vFor.spec.ts
@@ -1,60 +1,71 @@
 import { makeCompile } from './_utils'
 import {
+  type ForIRNode,
+  IRNodeTypes,
   transformElement,
   transformInterpolation,
   transformVFor,
+  transformVOn,
 } from '../../src'
+import { NodeTypes } from '@vue/compiler-dom'
 
 const compileWithVFor = makeCompile({
   nodeTransforms: [transformInterpolation, transformVFor, transformElement],
+  directiveTransforms: { on: transformVOn },
 })
 
 describe('compiler: v-for', () => {
   test('basic v-for', () => {
-    const { code, vaporHelpers, helpers } = compileWithVFor(
+    const { code, ir, vaporHelpers, helpers } = compileWithVFor(
       `<div v-for="item of items" @click="remove(item)">{{ item }}</div>`,
     )
 
     expect(code).matchSnapshot()
     expect(vaporHelpers).contains('createFor')
     expect(helpers.size).toBe(0)
-    // expect(ir.template).lengthOf(2)
-    // expect(ir.template).toMatchObject([
-    //   {
-    //     template: '<div></div>',
-    //     type: IRNodeTypes.TEMPLATE_FACTORY,
-    //   },
-    //   {
-    //     type: IRNodeTypes.FRAGMENT_FACTORY,
-    //   },
-    // ])
-    // expect(ir.operation).toMatchObject([
-    //   {
-    //     type: IRNodeTypes.IF,
-    //     id: 1,
-    //     condition: {
-    //       type: NodeTypes.SIMPLE_EXPRESSION,
-    //       content: 'ok',
-    //       isStatic: false,
-    //     },
-    //     positive: {
-    //       type: IRNodeTypes.BLOCK_FUNCTION,
-    //       templateIndex: 0,
-    //     },
-    //   },
-    //   {
-    //     type: IRNodeTypes.APPEND_NODE,
-    //     elements: [1],
-    //     parent: 0,
-    //   },
-    // ])
+    expect(ir.template).lengthOf(2)
+    expect(ir.template).toMatchObject([
+      {
+        template: '<div></div>',
+        type: IRNodeTypes.TEMPLATE_FACTORY,
+      },
+      {
+        type: IRNodeTypes.FRAGMENT_FACTORY,
+      },
+    ])
+    console.log(ir.operation)
 
-    // expect(ir.dynamic).toMatchObject({
-    //   id: 0,
-    //   children: { 0: { id: 1 } },
-    // })
+    expect(ir.operation).toMatchObject([
+      {
+        type: IRNodeTypes.FOR,
+        id: 1,
+        source: {
+          type: NodeTypes.SIMPLE_EXPRESSION,
+          content: 'items',
+        },
+        value: {
+          type: NodeTypes.SIMPLE_EXPRESSION,
+          content: 'item',
+        },
+        key: undefined,
+        index: undefined,
+        render: {
+          type: IRNodeTypes.BLOCK_FUNCTION,
+          templateIndex: 0,
+        },
+      },
+      {
+        type: IRNodeTypes.APPEND_NODE,
+        elements: [1],
+        parent: 0,
+      },
+    ])
 
-    // expect(ir.effect).toEqual([])
-    // expect((ir.operation[0] as IfIRNode).positive.effect).lengthOf(1)
+    expect(ir.dynamic).toMatchObject({
+      id: 0,
+      children: { 0: { id: 1 } },
+    })
+    expect(ir.effect).toEqual([])
+    expect((ir.operation[0] as ForIRNode).render.effect).lengthOf(1)
   })
 })
