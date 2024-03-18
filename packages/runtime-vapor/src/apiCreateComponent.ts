bdc1ee41ce2724e8bb3b975a21936ef6e4527fe1
Doctor Wu <doctorwu@moego.pet>
2024-03-18 17:53:01 +0800

    chore(runtime-vapor): remove unecessary code

--- packages/runtime-vapor/src/apiCreateComponent.ts
+++ packages/runtime-vapor/src/apiCreateComponent.ts
@@ -3,7 +3,7 @@ import {
   createComponentInstance,
   currentInstance,
 } from './component'
-import { type WithAttrsNode, setupComponent, withAttrsKey } from './apiRender'
+import { setupComponent } from './apiRender'
 import type { RawProps } from './componentProps'
 
 export function createComponent(
@@ -13,9 +13,6 @@ export function createComponent(
 ) {
   const current = currentInstance!
   const instance = createComponentInstance(comp, rawProps)
-  if (instance.block && withAttrs && !(withAttrsKey in instance.block)) {
-    ;(instance.block as WithAttrsNode)[withAttrsKey] = instance.uid
-  }
   setupComponent(instance, withAttrs)
 
   // register sub-component with current component for lifecycle management
