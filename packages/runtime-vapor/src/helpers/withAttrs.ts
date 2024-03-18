35499bd2515aaa16682661fd10753e6713416691
Doctor Wu <doctorwu@moego.pet>
2024-03-18 18:00:26 +0800

    feat(runtime-vapor): should not withAttrs when component set inheritAttrs false

--- packages/runtime-vapor/src/helpers/withAttrs.ts
+++ packages/runtime-vapor/src/helpers/withAttrs.ts
@@ -3,6 +3,7 @@ import type { RawProps } from '../componentProps'
 
 export function withAttrs(props: RawProps): RawProps {
   const instance = currentInstance!
+  if (instance.component.inheritAttrs === false) return props
   if (!props) return [() => instance.attrs]
   if (Array.isArray(props)) {
     return [() => instance.attrs, ...props]
