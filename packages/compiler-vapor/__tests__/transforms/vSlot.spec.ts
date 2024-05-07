0c0a131bddec71b154ac88d5981c7bb328027acf
Rizumu Ayaka <rizumu@ayaka.moe>
2024-05-08 02:04:47 +0800

    test: improve the test

--- packages/compiler-vapor/__tests__/transforms/vSlot.spec.ts
+++ packages/compiler-vapor/__tests__/transforms/vSlot.spec.ts
@@ -1,4 +1,5 @@
 import {
+  IRNodeTypes,
   transformChildren,
   transformElement,
   transformSlotOutlet,
@@ -29,17 +30,63 @@ const compileWithSlots = makeCompile({
 
 describe('compiler: transform slot', () => {
   test('implicit default slot', () => {
-    const { code } = compileWithSlots(`<Comp><div/></Comp>`)
+    const { ir, code } = compileWithSlots(`<Comp><div/></Comp>`)
     expect(code).toMatchSnapshot()
+
+    expect(ir.template).toEqual(['<div></div>'])
+    expect(ir.block.operation).toMatchObject([
+      {
+        type: IRNodeTypes.CREATE_COMPONENT_NODE,
+        id: 1,
+        tag: 'Comp',
+        props: [[]],
+        slots: {
+          default: {
+            type: IRNodeTypes.BLOCK,
+            dynamic: {
+              children: [{ template: 0 }],
+            },
+          },
+        },
+      },
+    ])
+    expect(ir.block.returns).toEqual([1])
+    expect(ir.block.dynamic).toMatchObject({
+      children: [{ id: 1 }],
+    })
   })
 
   test('named slots w/ implicit default slot', () => {
-    const { code } = compileWithSlots(
+    const { ir, code } = compileWithSlots(
       `<Comp>
         <template #one>foo</template>bar<span/>
       </Comp>`,
     )
     expect(code).toMatchSnapshot()
+
+    expect(ir.template).toEqual(['foo', 'bar', '<span></span>'])
+    expect(ir.block.operation).toMatchObject([
+      {
+        type: IRNodeTypes.CREATE_COMPONENT_NODE,
+        id: 4,
+        tag: 'Comp',
+        props: [[]],
+        slots: {
+          one: {
+            type: IRNodeTypes.BLOCK,
+            dynamic: {
+              children: [{ template: 0 }],
+            },
+          },
+          default: {
+            type: IRNodeTypes.BLOCK,
+            dynamic: {
+              children: [{}, { template: 1 }, { template: 2 }],
+            },
+          },
+        },
+      },
+    ])
   })
 
   test('nested slots', () => {
