afafd839ce7cb37c01fc6cca1c68376d27bf54b0
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-17 11:40:48 +0800

    feat(runtime-vapor): fallback component

--- packages/runtime-vapor/src/apiCreateComponent.ts
+++ packages/runtime-vapor/src/apiCreateComponent.ts
@@ -4,18 +4,31 @@ import {
   createComponentInstance,
   currentInstance,
 } from './component'
-import { setupComponent } from './apiRender'
-import type { RawProps } from './componentProps'
+import { type Block, setupComponent } from './apiRender'
+import {
+  type NormalizedRawProps,
+  type RawProps,
+  normalizeRawProps,
+  walkRawProps,
+} from './componentProps'
 import type { RawSlots } from './componentSlots'
 import { withAttrs } from './componentAttrs'
+import { isString } from '@vue/shared'
+import { renderEffect } from './renderEffect'
+import { normalizeBlock } from './dom/element'
+import { setDynamicProp } from './dom/prop'
 
 export function createComponent(
-  comp: Component,
+  comp: Component | string,
   rawProps: RawProps | null = null,
   slots: RawSlots | null = null,
   singleRoot: boolean = false,
   once: boolean = false,
 ): ComponentInternalInstance {
+  if (isString(comp)) {
+    return fallbackComponent(comp, rawProps, slots, singleRoot)
+  }
+
   const current = currentInstance!
   const instance = createComponentInstance(
     comp,
@@ -29,4 +42,44 @@ export function createComponent(
   current.comps.add(instance)
 
   return instance
+}
+
+function fallbackComponent(
+  comp: string,
+  rawProps: RawProps | null,
+  slots: RawSlots | null,
+  singleRoot: boolean,
+) {
+  // eslint-disable-next-line no-restricted-globals
+  const el = document.createElement(comp)
+
+  if (rawProps) {
+    rawProps = normalizeRawProps(rawProps)
+    renderEffect(() => {
+      walkRawProps(rawProps as NormalizedRawProps, (key, value, getter) => {
+        setDynamicProp(el, key, getter ? value() : value)
+      })
+    })
+  }
+
+  if (slots && slots.length) {
+    renderEffect(() => {
+      let block: Block | undefined
+
+      if (slots && slots.default) {
+        block = slots.default()
+      } else {
+        for (const slotFn of dynamicSlots!) {
+          const slot = slotFn()
+          if (slot.name === 'default') {
+            block = slot.fn()
+            break
+          }
+        }
+      }
+
+      if (block) el.append(...normalizeBlock(block))
+    })
+  }
+  return { __return: el, rawProps }
 }
