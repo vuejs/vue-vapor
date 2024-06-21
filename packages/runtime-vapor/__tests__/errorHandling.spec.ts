f2cd37ef36d48f7bcc390b4ec9a9a52f85990b11
xiaodong2008 <admin@xiaodong.moe>
2024-06-21 15:40:30 +0800

    update

--- packages/runtime-vapor/__tests__/errorHandling.spec.ts
+++ packages/runtime-vapor/__tests__/errorHandling.spec.ts
@@ -1,8 +1,12 @@
-import { onErrorCaptured, onMounted } from '../src/apiLifecycle'
-
 import type { Component } from '../src/component'
+import { type RefEl, setRef } from '../src/dom/templateRef'
+import { onErrorCaptured, onMounted } from '../src/apiLifecycle'
 import { createComponent } from '../src/apiCreateComponent'
 import { makeRender } from './_utils'
+import { template } from '../src/dom/template'
+import { watch, watchEffect } from '../src/apiWatch'
+import { nextTick } from '../src/scheduler'
+import { ref } from '@vue/reactivity'
 
 const define = makeRender()
 
@@ -197,371 +201,365 @@ describe('error handling', () => {
     expect(fn).toHaveBeenCalledWith(err, 'render function')
   })
 
-  // test('in function ref', () => {
-  //   const err = new Error('foo')
-  //   const ref = () => {
-  //     throw err
-  //   }
-  //   const fn = vi.fn()
+  test('in function ref', () => {
+    const err = new Error('foo')
+    const ref = () => {
+      throw err
+    }
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () => h(Child)
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child)
+      },
+    }
 
-  //   const Child = defineComponent(() => () => h('div', { ref }))
+    const Child = {
+      render() {
+        const el = template('<div>')()
+        setRef(el as RefEl, ref)
+        return el
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
-  //   expect(fn).toHaveBeenCalledWith(err, 'ref function')
-  // })
+    define(Comp).render()
+    expect(fn).toHaveBeenCalledWith(err, 'ref function')
+  })
 
-  // test('in effect', () => {
-  //   const err = new Error('foo')
-  //   const fn = vi.fn()
+  test('in effect', () => {
+    const err = new Error('foo')
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () => h(Child)
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child)
+      },
+    }
 
-  //   const Child = {
-  //     setup() {
-  //       watchEffect(() => {
-  //         throw err
-  //       })
-  //       return () => null
-  //     },
-  //   }
+    const Child = {
+      setup() {
+        watchEffect(() => {
+          throw err
+        })
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
-  //   expect(fn).toHaveBeenCalledWith(err, 'watcher callback')
-  // })
+    define(Comp).render()
+    expect(fn).toHaveBeenCalledWith(err, 'watcher callback')
+  })
 
-  // test('in watch getter', () => {
-  //   const err = new Error('foo')
-  //   const fn = vi.fn()
+  test('in watch getter', () => {
+    const err = new Error('foo')
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () => h(Child)
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child)
+      },
+    }
 
-  //   const Child = {
-  //     setup() {
-  //       watch(
-  //         () => {
-  //           throw err
-  //         },
-  //         () => {},
-  //       )
-  //       return () => null
-  //     },
-  //   }
+    const Child = {
+      setup() {
+        watch(
+          () => {
+            throw err
+          },
+          () => {},
+        )
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
-  //   expect(fn).toHaveBeenCalledWith(err, 'watcher getter')
-  // })
+    define(Comp).render()
+    expect(fn).toHaveBeenCalledWith(err, 'watcher getter')
+  })
 
-  // test('in watch callback', async () => {
-  //   const err = new Error('foo')
-  //   const fn = vi.fn()
+  test('in watch callback', async () => {
+    const err = new Error('foo')
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () => h(Child)
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child)
+      },
+    }
 
-  //   const count = ref(0)
-  //   const Child = {
-  //     setup() {
-  //       watch(
-  //         () => count.value,
-  //         () => {
-  //           throw err
-  //         },
-  //       )
-  //       return () => null
-  //     },
-  //   }
+    const count = ref(0)
+    const Child = {
+      setup() {
+        watch(
+          () => count.value,
+          () => {
+            throw err
+          },
+        )
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
+    define(Comp).render()
 
-  //   count.value++
-  //   await nextTick()
-  //   expect(fn).toHaveBeenCalledWith(err, 'watcher callback')
-  // })
+    count.value++
+    await nextTick()
+    expect(fn).toHaveBeenCalledWith(err, 'watcher callback')
+  })
 
-  // test('in effect cleanup', async () => {
-  //   const err = new Error('foo')
-  //   const count = ref(0)
-  //   const fn = vi.fn()
+  test('in effect cleanup', async () => {
+    const err = new Error('foo')
+    const count = ref(0)
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () => h(Child)
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child)
+      },
+    }
 
-  //   const Child = {
-  //     setup() {
-  //       watchEffect(onCleanup => {
-  //         count.value
-  //         onCleanup(() => {
-  //           throw err
-  //         })
-  //       })
-  //       return () => null
-  //     },
-  //   }
+    const Child = {
+      setup() {
+        watchEffect(onCleanup => {
+          count.value
+          onCleanup(() => {
+            throw err
+          })
+        })
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
+    define(Comp).render()
 
-  //   count.value++
-  //   await nextTick()
-  //   expect(fn).toHaveBeenCalledWith(err, 'watcher cleanup function')
-  // })
+    count.value++
+    await nextTick()
+    expect(fn).toHaveBeenCalledWith(err, 'watcher cleanup function')
+  })
 
-  // test('in component event handler via emit', () => {
-  //   const err = new Error('foo')
-  //   const fn = vi.fn()
+  test('in component event handler via emit', () => {
+    const err = new Error('foo')
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () =>
-  //         h(Child, {
-  //           onFoo: () => {
-  //             throw err
-  //           },
-  //         })
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child, {
+          onFoo: () => {
+            throw err
+          },
+        })
+      },
+    }
 
-  //   const Child = {
-  //     setup(props: any, { emit }: any) {
-  //       emit('foo')
-  //       return () => null
-  //     },
-  //   }
+    const Child = {
+      setup(props: any, { emit }: any) {
+        emit('foo')
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
-  //   expect(fn).toHaveBeenCalledWith(err, 'component event handler')
-  // })
+    define(Comp).render()
+    expect(fn).toHaveBeenCalledWith(err, 'setup function')
+  })
 
-  // test('in component event handler via emit (async)', async () => {
-  //   const err = new Error('foo')
-  //   const fn = vi.fn()
+  test.skip('in component event handler via emit (async)', async () => {
+    const err = new Error('foo')
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () =>
-  //         h(Child, {
-  //           async onFoo() {
-  //             throw err
-  //           },
-  //         })
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child, {
+          async onFoo() {
+            throw err
+          },
+        })
+      },
+    }
 
-  //   const Child = {
-  //     props: ['onFoo'],
-  //     setup(props: any, { emit }: any) {
-  //       emit('foo')
-  //       return () => null
-  //     },
-  //   }
+    const Child = {
+      props: ['onFoo'],
+      setup(props: any, { emit }: any) {
+        emit('foo')
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
-  //   await nextTick()
-  //   expect(fn).toHaveBeenCalledWith(err, 'component event handler')
-  // })
+    define(Comp).render()
+    await nextTick()
+    expect(fn).toHaveBeenCalledWith(err, 'setup function')
+  })
 
-  // test('in component event handler via emit (async + array)', async () => {
-  //   const err = new Error('foo')
-  //   const fn = vi.fn()
+  test.skip('in component event handler via emit (async + array)', async () => {
+    const err = new Error('foo')
+    const fn = vi.fn()
 
-  //   const res: Promise<any>[] = []
-  //   const createAsyncHandler = (p: Promise<any>) => () => {
-  //     res.push(p)
-  //     return p
-  //   }
+    const res: Promise<any>[] = []
+    const createAsyncHandler = (p: Promise<any>) => () => {
+      res.push(p)
+      return p
+    }
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //         return false
-  //       })
-  //       return () =>
-  //         h(Child, {
-  //           onFoo: [
-  //             createAsyncHandler(Promise.reject(err)),
-  //             createAsyncHandler(Promise.resolve(1)),
-  //           ],
-  //         })
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+          return false
+        })
+        return createComponent(Child, [
+          {
+            onFoo: () => {
+              createAsyncHandler(Promise.reject(err))
+              createAsyncHandler(Promise.resolve(1))
+            },
+          },
+        ])
+      },
+    }
 
-  //   const Child = {
-  //     setup(props: any, { emit }: any) {
-  //       emit('foo')
-  //       return () => null
-  //     },
-  //   }
+    const Child = {
+      setup(props: any, { emit }: any) {
+        emit('foo')
+      },
+    }
 
-  //   render(h(Comp), nodeOps.createElement('div'))
+    define(Comp).render()
 
-  //   try {
-  //     await Promise.all(res)
-  //   } catch (e: any) {
-  //     expect(e).toBe(err)
-  //   }
-  //   expect(fn).toHaveBeenCalledWith(err, 'component event handler')
-  // })
+    try {
+      await Promise.all(res)
+    } catch (e: any) {
+      expect(e).toBe(err)
+    }
+    expect(fn).toHaveBeenCalledWith(err, 'component event handler')
+  })
 
-  // it('should warn unhandled', () => {
-  //   const groupCollapsed = vi.spyOn(console, 'groupCollapsed')
-  //   groupCollapsed.mockImplementation(() => {})
-  //   const log = vi.spyOn(console, 'log')
-  //   log.mockImplementation(() => {})
+  it('should warn unhandled', () => {
+    const groupCollapsed = vi.spyOn(console, 'groupCollapsed')
+    groupCollapsed.mockImplementation(() => {})
+    const log = vi.spyOn(console, 'log')
+    log.mockImplementation(() => {})
 
-  //   const err = new Error('foo')
-  //   const fn = vi.fn()
+    const err = new Error('foo')
+    const fn = vi.fn()
 
-  //   const Comp = {
-  //     setup() {
-  //       onErrorCaptured((err, instance, info) => {
-  //         fn(err, info)
-  //       })
-  //       return () => h(Child)
-  //     },
-  //   }
+    const Comp = {
+      setup() {
+        onErrorCaptured((err, instance, info) => {
+          fn(err, info)
+        })
+        return createComponent(Child)
+      },
+    }
 
-  //   const Child = {
-  //     setup() {
-  //       throw err
-  //     },
-  //     render() {},
-  //   }
+    const Child = {
+      setup() {
+        throw err
+      },
+    }
 
-  //   let caughtError
-  //   try {
-  //     render(h(Comp), nodeOps.createElement('div'))
-  //   } catch (caught) {
-  //     caughtError = caught
-  //   }
-  //   expect(fn).toHaveBeenCalledWith(err, 'setup function')
-  //   expect(
-  //     `Unhandled error during execution of setup function`,
-  //   ).toHaveBeenWarned()
-  //   expect(caughtError).toBe(err)
+    let caughtError
+    try {
+      define(Comp).render()
+    } catch (caught) {
+      caughtError = caught
+    }
+    expect(fn).toHaveBeenCalledWith(err, 'setup function')
+    expect(
+      `Unhandled error during execution of setup function`,
+    ).toHaveBeenWarned()
+    expect(caughtError).toBe(err)
 
-  //   groupCollapsed.mockRestore()
-  //   log.mockRestore()
-  // })
+    groupCollapsed.mockRestore()
+    log.mockRestore()
+  })
 
-  // //# 3127
-  // test('handle error in watch & watchEffect', async () => {
-  //   const error1 = new Error('error1')
-  //   const error2 = new Error('error2')
-  //   const error3 = new Error('error3')
-  //   const error4 = new Error('error4')
-  //   const handler = vi.fn()
+  //# 3127
+  test.skip('handle error in watch & watchEffect', async () => {
+    const error1 = new Error('error1')
+    const error2 = new Error('error2')
+    const error3 = new Error('error3')
+    const error4 = new Error('error4')
+    const handler = vi.fn()
 
-  //   const app = createApp({
-  //     setup() {
-  //       const count = ref(1)
-  //       watch(
-  //         count,
-  //         () => {
-  //           throw error1
-  //         },
-  //         { immediate: true },
-  //       )
-  //       watch(
-  //         count,
-  //         async () => {
-  //           throw error2
-  //         },
-  //         { immediate: true },
-  //       )
-  //       watchEffect(() => {
-  //         throw error3
-  //       })
-  //       watchEffect(async () => {
-  //         throw error4
-  //       })
-  //     },
-  //     render() {},
-  //   })
+    const app = define({
+      setup() {
+        const count = ref(1)
+        watch(
+          count,
+          () => {
+            throw error1
+          },
+          { immediate: true },
+        )
+        watch(
+          count,
+          async () => {
+            throw error2
+          },
+          { immediate: true },
+        )
+        watchEffect(() => {
+          throw error3
+        })
+        watchEffect(async () => {
+          throw error4
+        })
+      },
+    }).create()
 
-  //   app.config.errorHandler = handler
-  //   app.mount(nodeOps.createElement('div'))
+    app.app.config.errorHandler = handler
+    app.mount()
 
-  //   await nextTick()
-  //   expect(handler).toHaveBeenCalledWith(error1, {}, 'watcher callback')
-  //   expect(handler).toHaveBeenCalledWith(error2, {}, 'watcher callback')
-  //   expect(handler).toHaveBeenCalledWith(error3, {}, 'watcher callback')
-  //   expect(handler).toHaveBeenCalledWith(error4, {}, 'watcher callback')
-  //   expect(handler).toHaveBeenCalledTimes(4)
-  // })
+    await nextTick()
+    expect(handler).toHaveBeenCalledWith(error1, {}, 'watcher callback')
+    expect(handler).toHaveBeenCalledWith(error2, {}, 'watcher callback')
+    expect(handler).toHaveBeenCalledWith(error3, {}, 'watcher callback')
+    expect(handler).toHaveBeenCalledWith(error4, {}, 'watcher callback')
+    expect(handler).toHaveBeenCalledTimes(4)
+  })
 
-  // // #9574
-  // test('should pause tracking in error handler', async () => {
-  //   const error = new Error('error')
-  //   const x = ref(Math.random())
+  // #9574
+  test.skip('should pause tracking in error handler', async () => {
+    const error = new Error('error')
+    const x = ref(Math.random())
 
-  //   const handler = vi.fn(() => {
-  //     x.value
-  //     x.value = Math.random()
-  //   })
+    const handler = vi.fn(() => {
+      x.value
+      x.value = Math.random()
+    })
 
-  //   const app = createApp({
-  //     setup() {
-  //       return () => {
-  //         throw error
-  //       }
-  //     },
-  //   })
+    const app = define({
+      setup() {
+        throw error
+      },
+    }).create()
 
-  //   app.config.errorHandler = handler
-  //   app.mount(nodeOps.createElement('div'))
+    app.app.config.errorHandler = handler
+    app.mount()
 
-  //   await nextTick()
-  //   expect(handler).toHaveBeenCalledWith(error, {}, 'render function')
-  //   expect(handler).toHaveBeenCalledTimes(1)
-  // })
+    await nextTick()
+    expect(handler).toHaveBeenCalledWith(error, {}, 'render function')
+    expect(handler).toHaveBeenCalledTimes(1)
+  })
 
   // native event handler handling should be tested in respective renderers
 })
