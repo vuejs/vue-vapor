1b6978c18cef08d6e1cc55a8c73639ed38ee8345
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-23 18:40:05 +0800

    ci: update release

--- scripts/release.js
+++ scripts/release.js
@@ -140,12 +140,12 @@ async function main() {
     const major = semver.major(currentVersion)
     let newVersion
 
+    const date = new Date()
+    const yyyy = date.getUTCFullYear()
+    const MM = (date.getUTCMonth() + 1).toString().padStart(2, '0')
     if (isCanary) {
       // The canary version string format is `3.yyyyMMdd.0` (or `3.yyyyMMdd.0-minor.0` for minor)
       // Use UTC date so that it's consistent across CI and maintainers' machines
-      const date = new Date()
-      const yyyy = date.getUTCFullYear()
-      const MM = (date.getUTCMonth() + 1).toString().padStart(2, '0')
       const dd = date.getUTCDate().toString().padStart(2, '0')
 
       const datestamp = `${yyyy}${MM}${dd}`
@@ -154,7 +154,7 @@ async function main() {
         newVersion = `${major}.${datestamp}.0-${args.tag}.0`
       }
     } else {
-      newVersion = `${major}.2024.0-${await getSha(true)}`
+      newVersion = `${major}.${yyyy}.${MM}-${await getSha(true)}`
     }
 
     // check the registry to avoid version collision
